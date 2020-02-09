4f6e376c80d3b6eafa9bc06b7e695d0dd5ec50f7
Alexey Novikov <novikov.aleksei.92@gmail.com>
2020-02-09 14:24:59 +0200

    Revert

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,12 @@
 plugins {
-    base
     kotlin("jvm") version "1.3.61"
-    jacoco
 }
 
 group = "org.konverter"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenLocal()
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
@@ -23,20 +19,5 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
-    }
-
-    jacocoTestReport {
-        subprojects.forEach {
-            sourceSets(it.sourceSets.main.get())
-        }
-
-        reports {
-            xml.isEnabled = true
-            xml.setDestination(file("${buildDir}/reports/jacoco/report.xml"))
-            csv.isEnabled = false
-            html.isEnabled = false
-        }
-
-        setDependsOn(listOf("test"))
     }
 }
