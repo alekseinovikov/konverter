dd62b77c401ad3db5081b33ec04be18e7929e7f4
Alexey Novikov <novikov.aleksei.92@gmail.com>
2020-02-09 14:20:33 +0200

    Added codecov

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
+    base
     kotlin("jvm") version "1.3.61"
+    jacoco
 }
 
 group = "org.konverter"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -19,5 +23,20 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    jacocoTestReport {
+        subprojects.forEach {
+            sourceSets(it.sourceSets.main.get())
+        }
+
+        reports {
+            xml.isEnabled = true
+            xml.setDestination(file("${buildDir}/reports/jacoco/report.xml"))
+            csv.isEnabled = false
+            html.isEnabled = false
+        }
+
+        setDependsOn(listOf("test"))
     }
 }
